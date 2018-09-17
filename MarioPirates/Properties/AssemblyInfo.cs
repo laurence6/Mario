9f7301efbba83f7470f0dfd1f0273d63e95ddc43
Xinyu Liu <liu.5658@osu.edu>
2018-09-17 19:11:02 -0400

    Update properties

--- MarioPirates/Properties/AssemblyInfo.cs
+++ MarioPirates/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sprint0")]
-[assembly: AssemblyProduct("Sprint0")]
+[assembly: AssemblyTitle("MarioPirates")]
+[assembly: AssemblyProduct("MarioPirates")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
