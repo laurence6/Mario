7009b9609e58ab713a91fcce5847c507c5e41b7c
Xinyu Liu <liu.5658@osu.edu>
2018-09-18 16:58:31 -0400

    Update EnemySpriteFactory

--- MarioPirates/Sprite/EnemySpriteFactory.cs
+++ MarioPirates/Sprite/EnemySpriteFactory.cs
@@ -1,33 +1,26 @@
-﻿using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+
 namespace MarioPirates
 {
-    public class EnemySpriteFactory
+    public sealed class EnemySpriteFactory
     {
+        public static EnemySpriteFactory Instance { get; } = new EnemySpriteFactory();
+
         private Texture2D koopaSpriteSheet;
         private Texture2D goombaSpriteSheet;
-        private static EnemySpriteFactory instance = new EnemySpriteFactory();
-        public static EnemySpriteFactory Instance
-        {
-            get
-            {
-                return instance;
-            }
-        }
+
         private EnemySpriteFactory()
-        {
-        }
+        { }
+
         public void LoadTextures(ContentManager content)
         {
             goombaSpriteSheet = content.Load<Texture2D>("goomba");
         }
+
         public ISprite createGoombaSprite()
         {
             return new GoombaSprite();
         }
     }
-
 }
