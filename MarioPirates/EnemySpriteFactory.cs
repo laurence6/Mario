7ca92e8707799a1efd442fea039cf8837d5bf109
Harry Hilson <40foutte@gmail.com>
2018-09-18 15:17:11 -0400

    enemy factory

---
+++ MarioPirates/EnemySpriteFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace MarioPirates
+{
+    public class EnemySpriteFactory
+    {
+        private Texture2D koopaSpriteSheet;
+        private Texture2D goombaSpriteSheet;
+        private static EnemySpriteFactory instance = new EnemySpriteFactory();
+        public static EnemySpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        private EnemySpriteFactory()
+        {
+        }
+        public void LoadTextures(ContentManager content)
+        {
+            goombaSpriteSheet = content.Load<Texture2D>("goomba");
+        }
+        public ISprite createGoombaSprite()
+        {
+            return new GoombaSprite();
+        }
+    }
+
+}
