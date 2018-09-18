ce83d0246ac97e058d57d16a3de5857532b58068
Xinyu Liu <liu.5658@osu.edu>
2018-09-18 17:45:50 -0400

    Update Pipe

--- MarioPirates/Sprite/Pipe.cs
+++ MarioPirates/Sprite/Pipe.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 
 namespace MarioPirates
 {
-    class Pipe : ISprite
+    public class Pipe : ISprite
     {
-        private Rectangle src, dest;
+        private Rectangle src, dst;
         private const int pipeWidth = 64, pipeHeight = 46, zoom = 2;
-        
-        public Pipe(int destX, int destY)
+
+        public Pipe(int dstX, int dstY)
         {
             src = new Rectangle(0, 0, pipeWidth, pipeHeight);
-            dest = new Rectangle(destX, destY, pipeWidth  * zoom, pipeHeight * zoom);
+            dst = new Rectangle(dstX, dstY, pipeWidth * zoom, pipeHeight * zoom);
         }
 
         public void Update()
@@ -21,7 +21,7 @@ namespace MarioPirates
 
         public void Draw(SpriteBatch spriteBatch, Dictionary<string, Texture2D> textures)
         {
-            spriteBatch.Draw(textures["pipeline"], dest, src, Color.White);
+            spriteBatch.Draw(textures["pipeline"], dst, src, Color.White);
         }
     }
 }
