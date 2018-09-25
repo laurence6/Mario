54bb7e7fce3b42605e1d8e0234362e5f2f279c30
Xinyu Liu <liu.5658@osu.edu>
2018-09-24 23:58:37 -0400

    Add Sprite

---
+++ MarioPirates/Sprite/SpriteFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MarioPirates
+{
+    public sealed class SpriteFactory
+    {
+        public static SpriteFactory Instance { get; private set; } = new SpriteFactory();
+
+        private SpriteFactory() { }
+
+        public Sprite CreateSprite(string spriteName)
+        {
+            switch (spriteName)
+            {
+                case "coins": return new Sprite("coins", new Point(30, 24), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 4));
+
+                case "flower": return new Sprite("flower", new Point(30, 20), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 4));
+
+                case "greenmushroom": return new Sprite("greenmushroom", new Point(28, 30), new Point(0, 0));
+
+                case "pipe": return new Sprite("pipeline", new Point(64, 64), new Point(0, 0));
+
+                case "redmushroom": return new Sprite("redmushroom", new Point(28, 30), new Point(0, 0));
+
+                case "stars": return new Sprite("stars", new Point(30, 24), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 4));
+
+                case "mario_big_idle_left": return new Sprite("bigmario", new Point(30, 32), new Point(150, 0));
+
+                // TODO
+
+                default: return null;
+            }
+        }
+
+        private Point[] GenerateFrameLocationArray(Point begin, Point delta, int count)
+        {
+            var arr = new Point[count];
+            for (var i = 0; i < count; i++)
+            {
+                arr[i].X = begin.X + delta.X * i;
+                arr[i].Y = begin.Y + delta.Y * i;
+            }
+            return arr;
+        }
+    }
+}
