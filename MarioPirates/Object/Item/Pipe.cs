15c2013142f497e13cb160a88cfacaa7f6631104
maggiebaum <baumgardner.85@osu.edu>
2018-09-24 20:10:11 -0400

    updated pipe class

---
+++ MarioPirates/Object/Item/Pipe.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace MarioPirates
+{
+    public class Pipe : GameObject
+    {
+        public Pipe(int dstX, int dstY)
+        {
+            location.X = dstX;
+            location.Y = dstY;
+            size = new Point(64, 46);
+            sprite = SpriteFactory.Instance.CreateSprite("pipe");
+        }
+    }
+}
