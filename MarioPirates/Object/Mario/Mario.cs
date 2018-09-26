f5545f0b06b21e5e6d1def6bd9b7521ab7f3f846
Xinyu Liu <liu.5658@osu.edu>
2018-09-25 21:06:01 -0400

    Fix Mario

--- MarioPirates/Object/Mario/Mario.cs
+++ MarioPirates/Object/Mario/Mario.cs
@@ -1,20 +1,14 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace MarioPirates
+﻿namespace MarioPirates
 {
     public class Mario : GameObject
     {
-        protected const int screenWidth = 800, screenHeight = 480;
-        protected const int textureFrameCount = 4;
-
         public MarioState State;
-
-        public Rectangle DrawSrc = new Rectangle(180, 0, MarioStateSmall.marioWidth, MarioStateSmall.marioHeight);
-        public Rectangle DrawDst = new Rectangle(0, 0, MarioStateSmall.marioWidth * MarioState.zoom, MarioStateSmall.marioHeight * MarioState.zoom);
 
         public Mario(int dstX, int dstY)
         {
-            State = new MarioStateSmallRightIdle(this, dstX, dstY);
+            location.X = dstX;
+            location.Y = dstY;
+            State = new MarioStateSmallRightIdle(this);
         }
     }
 }
