453ec3220698b9f8389d3b28fc5c0f3add2a59cf
Xinyu Liu <liu.5658@osu.edu>
2018-09-27 16:32:57 -0400

    Add Common

---
+++ MarioPirates/Common/Common.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MarioPirates
+{
+    internal sealed class Common
+    {
+        public static Point P(int X, int Y) => new Point(X, Y);
+    }
+}
--- MarioPirates/Sprite/SpriteFactory.cs
+++ MarioPirates/Sprite/SpriteFactory.cs
@@ -3,6 +3,8 @@ using System;
 
 namespace MarioPirates
 {
+    using static Common;
+
     internal sealed class SpriteFactory
     {
         public static SpriteFactory Instance { get; private set; } = new SpriteFactory();
@@ -14,46 +16,46 @@ namespace MarioPirates
             switch (spriteName)
             {
                 case "coins":
-                    return new Sprite("coins", new Point(30, 14), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 4));
+                    return new Sprite("coins", P(30, 14), GenerateFrameLocationArray(P(0, 0), P(30, 0), 4));
 
                 case "flower":
-                    return new Sprite("flower", new Point(30, 16), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 4));
+                    return new Sprite("flower", P(30, 16), GenerateFrameLocationArray(P(0, 0), P(30, 0), 4));
 
                 case "greenmushroom":
-                    return new Sprite("greenmushroom", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("greenmushroom", P(16, 16), P(0, 0));
 
                 case "pipe":
-                    return new Sprite("pipeline", new Point(32, 32), new Point(0, 0));
+                    return new Sprite("pipeline", P(32, 32), P(0, 0));
 
                 case "redmushroom":
-                    return new Sprite("redmushroom", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("redmushroom", P(16, 16), P(0, 0));
 
                 case "stars":
-                    return new Sprite("stars", new Point(30, 16), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 4));
+                    return new Sprite("stars", P(30, 16), GenerateFrameLocationArray(P(0, 0), P(30, 0), 4));
 
                 case "brickblock":
-                    return new Sprite("brickblock", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("brickblock", P(16, 16), P(0, 0));
 
                 case "brokenblock":
-                    return new Sprite("brokenblock", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("brokenblock", P(16, 16), P(0, 0));
 
                 case "brownblock":
-                    return new Sprite("brownblock", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("brownblock", P(16, 16), P(0, 0));
 
                 case "hiddenblock":
-                    return new Sprite("hiddenblock", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("hiddenblock", P(16, 16), P(0, 0));
 
                 case "orangeblock":
-                    return new Sprite("orangeblock", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("orangeblock", P(16, 16), P(0, 0));
 
                 case "questionblock":
-                    return new Sprite("questionblock", new Point(16, 16), new Point(0, 0));
+                    return new Sprite("questionblock", P(16, 16), P(0, 0));
 
                 case "goomba":
-                    return new Sprite("goomba", new Point(30, 16), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 2));
+                    return new Sprite("goomba", P(30, 16), GenerateFrameLocationArray(P(0, 0), P(30, 0), 2));
 
                 case "koopa":
-                    return new Sprite("koopa", new Point(30, 24), GenerateFrameLocationArray(new Point(0, 0), new Point(30, 0), 9));
+                    return new Sprite("koopa", P(30, 24), GenerateFrameLocationArray(P(0, 0), P(30, 0), 9));
             }
             Console.Error.WriteLine("Couldn't find sprite " + spriteName);
             return null;
@@ -64,79 +66,79 @@ namespace MarioPirates
             switch (spriteName)
             {
                 case "small_crouch_left":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(0, 0));
+                    return new Sprite("smallmario", P(30, 15), P(0, 0));
 
                 case "small_jump_left":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(0, 0));
+                    return new Sprite("smallmario", P(30, 15), P(0, 0));
 
                 case "small_run_left":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(120, 0), new Point(90, 0), new Point(60, 0), new Point(90, 0));
+                    return new Sprite("smallmario", P(30, 15), P(120, 0), P(90, 0), P(60, 0), P(90, 0));
 
                 case "small_idle_left":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(150, 0));
+                    return new Sprite("smallmario", P(30, 15), P(150, 0));
 
                 case "small_idle_right":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(180, 0));
+                    return new Sprite("smallmario", P(30, 15), P(180, 0));
 
                 case "small_run_right":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(210, 0), new Point(240, 0), new Point(270, 0), new Point(240, 0));
+                    return new Sprite("smallmario", P(30, 15), P(210, 0), P(240, 0), P(270, 0), P(240, 0));
 
                 case "small_jump_right":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(330, 0));
+                    return new Sprite("smallmario", P(30, 15), P(330, 0));
 
                 case "small_crouch_right":
-                    return new Sprite("smallmario", new Point(30, 15), new Point(330, 0));
+                    return new Sprite("smallmario", P(30, 15), P(330, 0));
 
                 case "big_crouch_left":
-                    return new Sprite("leftcrouchbigmario", new Point(16, 22), new Point(0, 0));
+                    return new Sprite("leftcrouchbigmario", P(16, 22), P(0, 0));
 
                 case "big_jump_left":
-                    return new Sprite("bigmario", new Point(30, 33), new Point(0, 0));
+                    return new Sprite("bigmario", P(30, 33), P(0, 0));
 
                 case "big_run_left":
-                    return new Sprite("bigmario", new Point(30, 33), new Point(120, 0), new Point(90, 0), new Point(60, 0), new Point(90, 0));
+                    return new Sprite("bigmario", P(30, 33), P(120, 0), P(90, 0), P(60, 0), P(90, 0));
 
                 case "big_idle_left":
-                    return new Sprite("bigmario", new Point(30, 33), new Point(150, 0));
+                    return new Sprite("bigmario", P(30, 33), P(150, 0));
 
                 case "big_idle_right":
-                    return new Sprite("bigmario", new Point(30, 33), new Point(180, 0));
+                    return new Sprite("bigmario", P(30, 33), P(180, 0));
 
                 case "big_run_right":
-                    return new Sprite("bigmario", new Point(30, 33), new Point(210, 0), new Point(240, 0), new Point(270, 0), new Point(240, 0));
+                    return new Sprite("bigmario", P(30, 33), P(210, 0), P(240, 0), P(270, 0), P(240, 0));
 
                 case "big_jump_right":
-                    return new Sprite("bigmario", new Point(30, 33), new Point(330, 0));
+                    return new Sprite("bigmario", P(30, 33), P(330, 0));
 
                 case "big_crouch_right":
-                    return new Sprite("rightcrouchbigmario", new Point(16, 22), new Point(0, 0));
+                    return new Sprite("rightcrouchbigmario", P(16, 22), P(0, 0));
 
                 case "fire_crouch_left":
-                    return new Sprite("leftcrouchfiremario", new Point(16, 22), new Point(0, 0));
+                    return new Sprite("leftcrouchfiremario", P(16, 22), P(0, 0));
 
                 case "fire_jump_left":
-                    return new Sprite("firemario", new Point(30, 33), new Point(0, 0));
+                    return new Sprite("firemario", P(30, 33), P(0, 0));
 
                 case "fire_run_left":
-                    return new Sprite("firemario", new Point(30, 33), new Point(120, 0), new Point(90, 0), new Point(60, 0), new Point(90, 0));
+                    return new Sprite("firemario", P(30, 33), P(120, 0), P(90, 0), P(60, 0), P(90, 0));
 
                 case "fire_idle_left":
-                    return new Sprite("firemario", new Point(30, 33), new Point(150, 0));
+                    return new Sprite("firemario", P(30, 33), P(150, 0));
 
                 case "fire_idle_right":
-                    return new Sprite("firemario", new Point(30, 33), new Point(180, 0));
+                    return new Sprite("firemario", P(30, 33), P(180, 0));
 
                 case "fire_run_right":
-                    return new Sprite("firemario", new Point(30, 33), new Point(210, 0), new Point(240, 0), new Point(270, 0), new Point(240, 0));
+                    return new Sprite("firemario", P(30, 33), P(210, 0), P(240, 0), P(270, 0), P(240, 0));
 
                 case "fire_jump_right":
-                    return new Sprite("firemario", new Point(30, 33), new Point(330, 0));
+                    return new Sprite("firemario", P(30, 33), P(330, 0));
 
                 case "fire_crouch_right":
-                    return new Sprite("rightcrouchfiremario", new Point(16, 22), new Point(0, 0));
+                    return new Sprite("rightcrouchfiremario", P(16, 22), P(0, 0));
 
                 case "dead":
-                    return new Sprite("deadmario", new Point(15, 14), new Point(0, 0));
+                    return new Sprite("deadmario", P(15, 14), P(0, 0));
             }
             Console.Error.WriteLine("Couldn't find sprite " + spriteName);
             return null;
