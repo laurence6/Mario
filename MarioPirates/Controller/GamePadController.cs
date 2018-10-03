a77b4bbf14664d10769cce779c91c4df5802ebb0
Xinyu Liu <liu.5658@osu.edu>
2018-10-03 18:22:40 -0400

    Update GamePadController

--- MarioPirates/Controller/GamePadController.cs
+++ MarioPirates/Controller/GamePadController.cs
@@ -9,50 +9,45 @@ namespace MarioPirates.Controller
 
     internal class GamePadController : IController
     {
-        private Dictionary<Buttons, IEvent>[] mapping;
+        private Dictionary<Buttons, IEvent>[] eventMapping;
+        private List<Buttons> enabledButton;
 
-        private GamePadState prevState = GamePad.GetState(PlayerIndex.One);
+        private GamePadState prevState, currState;
 
         public GamePadController()
         {
-            mapping = new Dictionary<Buttons, IEvent>[EnumValues<InputState>().Length];
-            for (var i = 0; i < mapping.Length; i++)
-                mapping[i] = new Dictionary<Buttons, IEvent>();
+            eventMapping = new Dictionary<Buttons, IEvent>[EnumValues<InputState>().Length];
+            for (var i = 0; i < eventMapping.Length; i++)
+                eventMapping[i] = new Dictionary<Buttons, IEvent>();
+            prevState = GamePad.GetState(PlayerIndex.One);
         }
 
         public void EnableButtonEvent(InputState state, Buttons[] buttons)
         {
             foreach (var b in buttons)
-                mapping[(int)state].Add(b, InputEventFactory.CreateButtonEvent(state, b));
+            {
+                eventMapping[(int)state].Add(b, InputEventFactory.CreateButtonEvent(state, b));
+                enabledButton.AddIfNotExist(b);
+            }
         }
 
         public void Update()
         {
-            IEvent e = null;
             var currState = GamePad.GetState(PlayerIndex.One);
             if (currState.IsConnected)
             {
-                foreach (var b in EnumValues<Buttons>())
-                    if (currState.IsButtonDown(b))
-                    {
-                        if (prevState.IsButtonDown(b))
-                        {
-                            if (mapping[(int)InputState.Hold].TryGetValue(b, out e))
-                                EventManager.Instance.EnqueueEvent(e);
-                        }
-                        else
-                        {
-                            if (mapping[(int)InputState.Down].TryGetValue(b, out e))
-                                EventManager.Instance.EnqueueEvent(e);
-                        }
-                    }
-                    else if (currState.IsButtonUp(b) && prevState.IsButtonDown(b))
-                    {
-                        if (mapping[(int)InputState.Up].TryGetValue(b, out e))
-                            EventManager.Instance.EnqueueEvent(e);
-                    }
+                enabledButton.ForEach(b =>
+                {
+                    var state = GetPrevButtonState(b) | GetCurrButtonState(b);
+
+                    if (eventMapping[(int)state].TryGetValue(b, out var e))
+                        EventManager.Instance.EnqueueEvent(e);
+                });
                 prevState = currState;
             }
         }
+
+        private InputState GetPrevButtonState(Buttons k) => prevState.IsButtonDown(k) ? InputState.Up : InputState.None;
+        private InputState GetCurrButtonState(Buttons k) => currState.IsButtonDown(k) ? InputState.Down : InputState.None;
     }
 }
