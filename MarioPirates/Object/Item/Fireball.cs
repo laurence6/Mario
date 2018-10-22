1976dc161eacca8aedd5a9886b10452fb0a9ee30
Harry Hilson <40foutte@gmail.com>
2018-10-22 19:23:10 -0400

    fireball bounce

---
+++ MarioPirates/Object/Item/Fireball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MarioPirates
+{
+    internal class Fireball : GameObjectRigidBody
+    {
+        private const int fireballwidth = 10, fireballheight=10;
+        public Fireball()
+        {
+            RigidBody.Mass = .05f;
+            RigidBody.Motion = MotionEnum.Dynamic;
+            Sprite = SpriteFactory.Ins.CreateSprite("fireball");
+        }
+        public override void Postcollide(GameObjectRigidBody other, CollisionSide side)
+        {
+            if (other.RigidBody.Motion == MotionEnum.Static) {
+                if (side == CollisionSide.Top)
+                {
+                    RigidBody.Velocity = new Vector2(RigidBody.Velocity.X, 25);
+                }             
+            }
+        }
+    }
+
+}
