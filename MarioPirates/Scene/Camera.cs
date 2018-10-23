0fb17a7f0b985d705b5334df6a678b514db36a7c
Xinyu Liu <liu.5658@osu.edu>
2018-10-23 00:30:16 -0400

    Add Camera

---
+++ MarioPirates/Scene/Camera.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MarioPirates
+{
+    internal class Camera
+    {
+        public Matrix Transform { get; private set; }
+
+        public Camera() : this(Vector2.Zero)
+        {
+        }
+
+        public Camera(Vector2 location)
+        {
+            LookAt(location);
+        }
+
+        public void LookAt(Vector2 location)
+        {
+            Transform = Matrix.CreateTranslation(new Vector3(-location, 0f));
+        }
+    }
+}
