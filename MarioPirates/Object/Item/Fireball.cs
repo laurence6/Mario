42ad817ceb65213be10edbb35a96e5ab32b5ba0b
Xinyu Liu <liu.5658@osu.edu>
2018-10-23 00:21:05 -0400

    Fix Fireball

--- MarioPirates/Object/Item/Fireball.cs
+++ MarioPirates/Object/Item/Fireball.cs
@@ -1,24 +1,27 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+
 namespace MarioPirates
 {
     internal class Fireball : GameObjectRigidBody
     {
-        private const int fireballwidth = 10, fireballheight=10;
-        public Fireball()
+        private const int fireballwidth = 10, fireballheight = 10;
+
+        public Fireball(int dstX, int dstY) : base(dstX, dstY, fireballwidth * 2, fireballheight * 2)
         {
             RigidBody.Mass = .05f;
             RigidBody.Motion = MotionEnum.Dynamic;
             Sprite = SpriteFactory.Ins.CreateSprite("fireball");
         }
-        public override void Postcollide(GameObjectRigidBody other, CollisionSide side)
+
+        public override void PostCollide(GameObjectRigidBody other, CollisionSide side)
         {
-            if (other.RigidBody.Motion == MotionEnum.Static) {
+            if (other.RigidBody.Motion == MotionEnum.Static)
+            {
                 if (side == CollisionSide.Top)
                 {
                     RigidBody.Velocity = new Vector2(RigidBody.Velocity.X, 25);
-                }             
+                }
             }
         }
     }
-
 }
