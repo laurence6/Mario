e8207888fc18f9b9eb34881aca89fed858fba5e0
Lok <huang.2870@osu.edu>
2018-11-07 22:11:38 -0500

    Add timer

---
+++ MarioPirates/GamePlay/Timer.cs
@@ -0,0 +1,26 @@
+﻿namespace MarioPirates
+{
+    class Timer
+    {
+        private float originalTime;
+        private uint timeLimit;
+        private float timeLeft;
+        
+        public static readonly Timer Ins = new Timer();
+
+        private Timer() { }
+
+        public uint Value => (timeLeft = (timeLimit - Time.Now + originalTime) / 1000) > 0 ? (uint) timeLeft : 0;
+
+        public void Reset(float originalTime)
+        {
+            this.originalTime = originalTime;
+        }
+
+        public void Reset(float originalTime, uint timeLimit)
+        {
+            this.originalTime = originalTime;
+            this.timeLimit = timeLimit;
+        }
+    }
+}
