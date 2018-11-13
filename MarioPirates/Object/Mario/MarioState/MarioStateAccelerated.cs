a7b5d25b3ac7afe33492efff5513f224c85cd852
Xinyu Liu <liu.5658@osu.edu>
2018-11-13 12:34:19 -0500

    Fix

--- MarioPirates/Object/Mario/MarioState/MarioStateAccelerated.cs
+++ MarioPirates/Object/Mario/MarioState/MarioStateAccelerated.cs
@@ -2,11 +2,9 @@ namespace MarioPirates
 {
     internal class MarioStateAccelerated
     {
-        public bool IsAccelerated { get; private set; }
+        public bool IsAccelerated { get; set; }
 
         public MarioStateAccelerated(bool isAccelerated = false) => IsAccelerated = isAccelerated;
-
-        public void SetAccelerated(bool isAccelerated) => IsAccelerated = isAccelerated;
 
         public float VelocityMultiplier => IsAccelerated ? Constants.ACCELERATING_MARIO_MULTIPLIER : Constants.NONACCELERATING_MARIO_MULTIPLIER;
     }
--- MarioPirates/Object/Mario/MarioState/MarioStateTransiting.cs
+++ MarioPirates/Object/Mario/MarioState/MarioStateTransiting.cs
@@ -2,12 +2,8 @@
 {
     internal class MarioStateTransiting
     {
-        public bool IsTransiting { get; private set; }
+        public bool IsTransiting { get; set; }
 
         public MarioStateTransiting(bool isTransiting = false) => IsTransiting = isTransiting;
-
-        public void SetTransiting(bool isTransiting) => IsTransiting = isTransiting;
-
-        public MarioStateEnum State => IsTransiting ? MarioStateEnum.Transiting : MarioStateEnum.NonTransiting;
     }
 }
