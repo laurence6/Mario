fc2d5d71ab9951bf73e60a03877297ef36a151c7
Xinyu Liu <liu.5658@osu.edu>
2018-12-01 15:25:43 -0500

    Remove comments

--- MarioPirates/GamePlay/GameMario.cs
+++ MarioPirates/GamePlay/GameMario.cs
@@ -21,16 +21,11 @@ namespace MarioPirates
             Content.RootDirectory = Constants.CONTENT_ROOT;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            graphics.IsFullScreen = false;
             base.Initialize();
+
+            graphics.IsFullScreen = false;
 
             spriteBatch = new SpriteBatch(GraphicsDevice);
             SpriteFactory.Ins.LoadContent(Content);
@@ -40,10 +35,6 @@ namespace MarioPirates
             GameOverReset();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -52,20 +43,11 @@ namespace MarioPirates
             AudioManager.Ins.LoadContent(Content);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             Content.Unload();
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (triggerGameOver)
@@ -89,10 +71,6 @@ namespace MarioPirates
             State.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             State.Draw(spriteBatch);
