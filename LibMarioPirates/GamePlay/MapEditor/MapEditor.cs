20646580a7e33b1b6ab30151c42038bb474032c4
Xinyu Liu <liu.5658@osu.edu>
2018-12-02 01:48:37 -0500

    MapEditor

---
+++ LibMarioPirates/GamePlay/MapEditor/MapEditor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace MarioPirates
+{
+    using Controller;
+
+    internal class MapEditor : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public List<IController> Controllers { get; } = new List<IController>();
+
+        public MapEditor()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = Constants.CONTENT_ROOT;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            graphics.IsFullScreen = false;
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteFactory.Ins.LoadContent(Content);
+            AudioManager.Ins.LoadContent(Content);
+        }
+
+        protected override void UnloadContent()
+        {
+            Content.Unload();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+        }
+    }
+}
---
+++ MapEditor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MarioPirates
+{
+#if WINDOWS || LINUX
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            new MapEditor().Run();
+        }
+    }
+#endif
+}
