09ab7e675b109bd5abdac3e506da20ac177a5579
Xinyu Liu <liu.5658@osu.edu>
2018-12-02 00:51:47 -0500

    Refactor

--- MarioPirates/Program.cs
+++ MarioPirates/Program.cs
@@ -3,19 +3,12 @@
 namespace MarioPirates
 {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         private static void Main()
         {
-            using (var game = new GameMario())
-                game.Run();
+            new GameMario().Run();
         }
     }
 #endif
